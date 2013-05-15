81776476fa138a46fedafda3b4932e7640e440fd
vincentstorme <storme.vincent@gmail.com>
2013-05-15 09:52:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stoverflow::Application.config.secret_token = 'd892d448efc2ab4fd9fc20175344cb2f6be2b1535b0b4e5c59861bfd3e733980f391ead571c930b18a6240da3ce5b3fddae3546643bb78e5b4a02183d7f6956b'
