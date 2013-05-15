bb9f97fbf4369724ae6674bc6831fc7ebb85cb2c
vincentstorme <storme.vincent@gmail.com>
2013-05-15 16:27:33 -0700

    uniqueness validation status => pending

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,45 +3,45 @@ require 'spec_helper'
 describe User do
   let(:user) { build(:user) }
 
-  it 'validates the length of a good name' do
-    user.should be_valid
-  end
-  it 'validates the length of a bad name' do
-    user = build(:user, :name => '')
-    user.should_not be_valid
-  end
+  describe '#Validations' do
+    it 'validates the length of a good name' do
+      user.should be_valid
+    end
+    it 'validates the length of a bad name' do
+      user = build(:user, :name => '')
+      user.should_not be_valid
+    end
 
 
-  it 'validates the length of a good email' do
-    user.should be_valid
-  end
-  it 'validates the length of a bad email' do
-    user = build(:user, :email => '')
-    user.should_not be_valid
-  end
+    it 'validates the length of a good email' do
+      user.should be_valid
+    end
+    it 'validates the length of a bad email' do
+      user = build(:user, :email => '')
+      user.should_not be_valid
+    end
 
 
-  it 'validates the length of a good password' do
-    user.should be_valid
-  end
-  it 'validates the length of a bad password' do
-    user = build(:user, :password => '')
-    user.should_not be_valid
-  end
+    it 'validates the length of a good password' do
+      user.should be_valid
+    end
+    it 'validates the length of a bad password' do
+      user = build(:user, :password => '')
+      user.should_not be_valid
+    end
 
 
-  it 'validates the format of a good email' do
-    user.should be_valid
-  end
-  it 'validates the format of a bad email' do
-    user = build(:user, :email => 'Pat Smells')
-    user.should_not be_valid
-  end
+    it 'validates the format of a good email' do
+      user.should be_valid
+    end
+    it 'validates the format of a bad email' do
+      user = build(:user, :email => 'Pat Smells')
+      user.should_not be_valid
+    end
 
 
-  it 'validates the uniqueness of a bad email' do
-    build(:user, :email => 'PatSmells')
-    user = build(:user, :email => 'PatSmells')
-    user.should_not be_valid
+    it 'validates the uniqueness of a bad email' do
+      pending
+    end
   end
 end
